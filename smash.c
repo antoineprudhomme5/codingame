fa7a49c6f6635c1ce4ddf6eda78ba2f2b2f7b278
prudywsh <antoineprudhomme5@gmail.com>
2016-06-29 11:42:32 +0200

    smash : lvl 2, but the code have some bugs

--- smash.c
+++ smash.c
@@ -23,6 +23,8 @@ char    my_grid[HEIGHT][WIDTH];         // my grid
 char    opponent_grid[HEIGHT][WIDTH];   // opponent grid
 char    top_colors[WIDTH];              // contains the colors at the top of each grid column
 
+int		response[2];					// [0] => column; [1] => rotation;
+
 // MAIN
 
 int main()
@@ -31,8 +33,8 @@ int main()
     
     while (1) {
         
-        for (int i = 0; i < NEXTB; i++) {   // 8 next blocks group 
-            scanf("%d%d", &next_blocks[i][0], &next_blocks[i][1]); // main color, second color
+        for (int i = 0; i < NEXTB; i++) {   					   	// 8 next blocks group 
+            scanf("%d%d", &next_blocks[i][0], &next_blocks[i][1]); 	// main color, second color
         }
         
         scanf("%d", &my_score);             // read my current score
@@ -54,7 +56,8 @@ int main()
         find_top_colors();
         display_top_colors();
         
-        printf("%d\n", drop_position());                      // the column in which to drop the blocks
+		drop_position();								// update the response array
+        printf("%d %d\n", response[0], response[1]);	// the column in which to drop the blocks
     }
 
     return 0;
@@ -65,37 +68,126 @@ int main()
 /**
  * Calculate the best position to drop the blocks
  */ 
-int drop_position()
+void drop_position()
 {
-    char color[1];
-    sprintf(color, "%d", next_blocks[0][0]);
-    int find = 0;
-    int position = 0;
-    int pos_empty = 0;
+	// next_blocks[0][0] & next_blocks[0][1]
+	int sbs_color_empty[2] = {-1, -1};					// side by side : color and empty ( [0] => column; [1] => rotation; )
+	int sbs_empty[2] = {-1, -1};						// two empty location side by side
+    int color1_pos = -1;								// position of a location which correspond to the 1 color
+	int color2_pos = -1;								// position of a location which correspond to the 2 color
+    int pos_empty = -1;									// position of an empty location
     int i = 0;
-    
-    while(i < WIDTH && !find)
+    int found = 0;
+	
+    while(i < WIDTH && !found)
     {
-        if(top_colors[i] == '.' || top_colors[i] == '0')
-        {
-            pos_empty = i;
-        }
-        else if(top_colors[i] == color[0])
-        {
-            find = 1;
-            position = i;
-        }
-        i++;
+	
+		if(i > 0) 
+		{
+			// color [0] at i-1 and color[1] at i 
+			if(top_colors[i-1] == next_blocks[0][0] && top_colors[i] == next_blocks[0][1])
+			{
+				 response[0] = i-1;
+				 response[1] = 0;
+				 found = 1;
+			}
+			// color [1] at i-1 and color[0] at i
+			else if(top_colors[i-1] == next_blocks[0][1] && top_colors[i] == next_blocks[0][0])
+			{
+				 response[0] = i;
+				 response[1] = 2;
+				 found = 1;
+			} 
+			// color [0] at i-1, empty at i
+			else if((top_colors[i-1] == next_blocks[0][0] && top_colors[i] == '.') || (top_colors[i-1] == next_blocks[0][0] && top_colors[i] == '0'))
+			{
+				sbs_color_empty[0] = i-1;
+				sbs_color_empty[1] = 0;
+			}
+			// color [1] at i-1, empty at i
+			else if((top_colors[i-1] == next_blocks[0][1] && top_colors[i] == '.') || (top_colors[i-1] == next_blocks[0][1] && top_colors[i] == '0'))
+			{
+				sbs_color_empty[0] = i-1;
+				sbs_color_empty[1] = 0;
+			}
+			// empty at i-1 and color[0] at i
+			else if((top_colors[i] == next_blocks[0][0] && top_colors[i-1] == '.') || (top_colors[i] == next_blocks[0][0] && top_colors[i-1] == '0'))
+			{
+				sbs_color_empty[0] = i;
+				sbs_color_empty[1] = 2;
+			}
+			// empty at i-1 and color[1] at i 
+			else if((top_colors[i] == next_blocks[0][1] && top_colors[i-1] == '.') || (top_colors[i] == next_blocks[0][1] && top_colors[i-1] == '0'))
+			{
+				sbs_color_empty[0] = i;
+				sbs_color_empty[1] = 2;
+			}
+			// empty at i and empty at i-1
+			else if(top_colors[i] == '.' || top_colors[i] == '0')
+			{
+				if(top_colors[i-1] == '.' || top_colors[i-1] == '0')
+				{
+					sbs_empty[0] = i;
+					sbs_empty[1] = 2;
+				}
+			}
+		}
+		
+		if(!found) 
+		{
+			if(top_colors[i] == '.' || top_colors[i] == '0')
+			{
+				pos_empty = i;
+			}
+			else if(top_colors[i] == next_blocks[0][0])
+			{
+				color1_pos = i;
+			}
+			else if(top_colors[i] == next_blocks[0][1])
+			{
+				color2_pos = i;
+			}
+		
+			i++;
+		}
     }
     
-    if(find == 1)
-    {
-        return position;   
-    }
-    else
-    {
-        return pos_empty;
-    }
+    // if no perfect response
+	if(!found)
+	{
+		// check if there is a empty location next to a color of next_blocks[0]
+		if(sbs_color_empty[0] > -1 && sbs_color_empty[1] > -1)
+		{
+			response[0] = sbs_color_empty[0];
+			response[1] = sbs_color_empty[1];
+		} 
+		// check if there is two empty location side by side
+		else if(sbs_empty[0] > -1 && sbs_empty[1] > -1)
+		{
+			response[0] = sbs_empty[0];
+			response[1] = sbs_empty[1];
+		}
+		else if(pos_empty > -1)
+		{
+			response[0] = pos_empty;
+			response[1] = 1;			// next step => look to the next_blocks color ( to write 1 or 3 by looking to the next colors )
+		}
+		else if(color1_pos > -1)
+		{
+			response[0] = color1_pos;
+			response[1] = 1;
+		}
+		else if(color2_pos > -1)
+		{
+			response[0] = color2_pos;
+			response[1] = 3;
+		}
+		else
+		{
+			response[0] = 0;
+			response[1] = 1;
+		}
+	}
 }
 
 /**
