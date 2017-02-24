f45d4d6059468e2be315bc95b1f231323e72fa67
prudywsh <antoineprudhomme5@gmail.com>
2017-02-24 21:27:46 +0100

    magic stones updated

--- community/magic_stones.py
+++ community/magic_stones.py
@@ -1,5 +1,18 @@
+class Node():
+    def __init__(self, key=None, next=None):
+        self.key = key
+        self.next = next
+
+def insert_node(head, data):
+    if head is None:
+        head = Node(key=data)
+    else:
+        head.next = insert_node(head.next, data)
+    return head
+
 d = {}
 n = int(input())
+stones = 0
 # read the stones level
 for i in input().split():
     i = int(i)
@@ -7,33 +20,33 @@ for i in input().split():
         d[i] += 1
     else:
         d[i] = 1
-
-stones = 0
-print(d)
-# store levels here, because new levels can be added dynamically
-levels = sorted(d)
-nb_levels = len(levels)
-level_index = 0
-# while there are levels to checks
-while level_index < nb_levels:
-    level = levels[level_index]
-    # how many stones of this level can we combine ?
-    to_combine = d[level]
-    if to_combine % 2 == 1:
-        to_combine -= 1
-        stones += 1
-    # search the level of the final stone
-    final_level = level
-    while to_combine > 1:
-        to_combine /= 2
-        final_level += 1
-    # increment or create the final level in the dict
-    if final_level in d:
-        d[final_level] += 1
+# build a linked list from the dict
+levels = sorted(d.keys())
+levels_list = None
+for i in range(0, len(levels)):
+    levels_list = insert_node(levels_list, levels[i])
+# go throught the list
+while levels_list != None:
+    # if we cant combine stones with this level
+    if d[levels_list.key] < 2:
+        # count the stones and remove this node from the linked list
+        stones += d[levels_list.key]
+        levels_list = levels_list.next
     else:
-        d[final_level] = 1
-        levels = sorted(d)
-        nb_levels += 1
-    level_index += 1
+        # count how many stones we can combine
+        to_combine = d[levels_list.key]
+        if to_combine % 2 == 1:
+            to_combine -= 1
+            stones += 1
+        # update the next level in dict
+        if levels_list.key + 1 in d:
+            d[levels_list.key + 1] += to_combine // 2
+        else:
+            d[levels_list.key + 1] = to_combine // 2
+        # update the list to go to the next level
+        if levels_list.next != None and levels_list.next.key == (levels_list.key + 1):
+            levels_list = levels_list.next
+        else:
+            levels_list.key += 1
 
 print(stones)
