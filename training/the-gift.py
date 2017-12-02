15c06b81deab8ea917b9bec083e8bd3d94021eec
prudywsh <antoineprudhomme5@gmail.com>
2017-12-02 22:47:28 +0100

    the gift: clean solution

--- training/the-gift.py
+++ training/the-gift.py
@@ -3,28 +3,11 @@ from math import floor
 n, price = map(int, (input(), input()))
 budgets = sorted(int(input()) for _ in range(n))
 
-# if not enought money, print impossible
 if sum(budgets) < price:
+    # if not enought money, print impossible
     print("IMPOSSIBLE")
 else:
-    contributions = [0]*n
-
-    i = 0
-    while i < n:
-        d = price / (n-i)
-        if d > budgets[i]:
-            contributions[i] = budgets[i]
-            price -= contributions[i]
-            i += 1
-        elif floor(d) != d:
-            contributions[i] = floor(d)
-            price -= contributions[i]
-            i += 1
-        else:
-            d = int(d)
-            while i < n:
-                contributions[i] = d
-                i += 1
-
-    for contribution in contributions:
-        print(contribution)
+    for i in range(n):
+        d = min(price // (n-i), budgets[i])
+        price -= d
+        print(d)
