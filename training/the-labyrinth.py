b92b3e0602845a326439d2c9870913c9472f6626
prudywsh <antoineprudhomme5@gmail.com>
2017-12-14 00:09:10 +0100

    the labyrinth: bugs fixed + now kirk can reach the command room using a* !

--- training/the-labyrinth.py
+++ training/the-labyrinth.py
@@ -1,45 +1,18 @@
 import sys
 import math
-from collections import deque
+from collections import deque, namedtuple
 
-turn = 1
+# define the Point namedtuple
+Point = namedtuple('Point', ['r', 'c'])
 
-class Cell(object):
-    def __init__(self, value):
-        self.value = value
-        self.score = 0
+def find_neighbours(arr, cell):
+    neighbours = [
+        Point(r=cell[0], c=cell[1]+1),
+        Point(r=cell[0], c=cell[1]-1),
+        Point(r=cell[0]+1, c=cell[1]),
+        Point(r=cell[0]-1, c=cell[1])]
 
-def heuristic(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-def find_direction(current, target):
-    if target[0] > current[0]:
-        return "DOWN"
-    if target[1] > current[1]:
-        return "RIGHT"
-    if target[0] < current[0]:
-        return "UP"
-    if target[1] < current[1]:
-        return "LEFT"
-
-def sort_insertion(arr, value):
-    if len(arr) == 0:
-        return [value]
-    if arr[0] < value:
-        return [value] + arr
-
-    i = 0
-    while i < len(arr):
-        if arr[i] < value:
-            i += 1
-    return arr[::i] + [value] + arr[i::]
-
-def find_cell(arr, target):
-    for row in range(len(arr)):
-        for col, cell in enumerate(arr[row]):
-            if cell.value == target:
-                return (row, col)
-    return None
+    return [neighbour for neighbour in neighbours if is_cell_reachable(arr, neighbour)]
 
 def is_cell_reachable(arr, cell):
     return (cell[0] >= 0
@@ -48,14 +21,8 @@ def is_cell_reachable(arr, cell):
             and cell[1] < len(arr[0])
             and arr[cell[0]][cell[1]].value != '#')
 
-def find_neighbours(arr, cell):
-    neighbours = [
-            (cell[0], cell[1]+1),
-            (cell[0], cell[1]-1),
-            (cell[0]+1, cell[1]),
-            (cell[0]-1, cell[1])]
-
-    return [neighbour for neighbour in neighbours if is_cell_reachable(arr, neighbour)]
+def heuristic(a, b):
+    return abs(a.r - b.r) + abs(a.c - b.c)
 
 def a_star(arr, start, target):
     # cells already evaluated
@@ -72,8 +39,16 @@ def a_star(arr, start, target):
     f_score[start] = heuristic(start, target)
 
     while len(open_set):
-        current = open_set.pop()
-        if current == target:
+
+        # find the cell with the lowest f score
+        lowestIndex = 0
+        for i in range(1, len(open_set)):
+            if f_score[open_set[i]] < f_score[open_set[lowestIndex]]:
+                lowestIndex = i
+
+        current = open_set.pop(lowestIndex)
+
+        if current.r == target.r and current.c == target.c:
             # build and return the path
             path = [current]
             while current in came_from:
@@ -89,14 +64,14 @@ def a_star(arr, start, target):
                 continue
             # check if not discovered
             if neighbour not in open_set:
-                open_set = sort_insertion(open_set, neighbour)
+                open_set.append(neighbour)
 
-                tempG = g_score[current] + 1
-                if neighbour not in g_score or tempG < g_score[neighbour]:
-                    # better path found
-                    came_from[neighbour] = current
-                    g_score[neighbour] = tempG
-                    f_score[neighbour] = tempG + heuristic(neighbour, target)
+            tempG = g_score[current] + 1
+            if neighbour not in g_score or tempG < g_score[neighbour]:
+                # better path found
+                came_from[neighbour] = current
+                g_score[neighbour] = tempG
+                f_score[neighbour] = g_score[neighbour] + heuristic(neighbour, target)
 
     return False
 
@@ -121,50 +96,84 @@ def BFS(grid, start, target):
 
         # queue each unvisited neighbours
         neighbours = find_neighbours(grid, cell)
-        if turn == 6:
-            print(neighbours)
         for neighbour in neighbours:
             if is_valid_cell(neighbour):
                 queue.append(neighbour)
 
                 parents[neighbour] = cell
 
+class Cell(object):
+    def __init__(self, value):
+        self.value = value
+        self.score = 0
+
+class Grid(object):
+    def __init__(self, nb_rows, nb_cols, alarm):
+        self.nb_rows = nb_rows
+        self.nb_cols = nb_cols
+        self.alarm = alarm
+        self.grid = [None] * nb_rows
+        self.CR_pos = None
+        self.kirk_pos = None
+        self.CR_reached = False
+
+    def update_row(self, row_index, row):
+        self.grid[row_index] = row
+        if not self.CR_pos:
+            for col, cell in enumerate(self.grid[row_index]):
+                if cell.value == 'C':
+                    self.CR_pos = Point(r=row_index, c=col)
+                    break
+
+    def find_cell(self, arr, target):
+        for row in range(self.nb_rows):
+            for col, cell in enumerate(self.grid[row]):
+                if cell.value == target:
+                    return (row, col)
+        return None
+
+    def find_direction(self, target):
+        if target.r > self.kirk_pos.r:
+            return "DOWN"
+        if target.c > self.kirk_pos.c:
+            return "RIGHT"
+        if target.r < self.kirk_pos.r:
+            return "UP"
+        if target.c < self.kirk_pos.c:
+            return "LEFT"
+
 # rows:     number of rows.
 # cols:     number of columns.
 # alarm:    number of rounds between the time the alarm countdown is activated and
 #           the time the alarm goes off.
-rows, cols, alarm = [int(i) for i in input().split()]
-# True if krik reached the command room
-CR_reached = False
+nb_rows, nb_cols, alarm = [int(i) for i in input().split()]
+
+grid = Grid(nb_rows, nb_cols, alarm)
 
 # game loop
 while True:
-    # kirk_row: row where Kirk is located.
-    # kirk_col: column where Kirk is located.
+    # update kirk pos
     kirk_row, kirk_col = [int(i) for i in input().split()]
-    kirk_pos = (kirk_row, kirk_col)
-    # read the grid
-    grid = []
-    for i in range(rows):
-        grid.append([Cell(v) for v in list(input())])
-    # search if C has been discovered
-    C_pos = find_cell(grid, 'C')
-    # check if kirk reached the command room
-    if not CR_reached and C_pos == kirk_pos:
-        CR_reached = True
-    # if C hasn't been discovered, continue to search
-    # (use BFS to find the closest cell to discover)
-    if C_pos == None:
-        next_cell = BFS(grid, kirk_pos, "?")[2]
-    # else, if C has been discovered and kirk didn't reach it, go to C
-    elif not CR_reached:
-        next_cell = (kirk_row, kirk_col+1)
-        #next_cell = a_star(grid, kirk_pos, C_pos)
-    else:
-        next_cell = (kirk_row, kirk_col-1)
+    grid.kirk_pos = Point(r=kirk_row, c=kirk_col)
+    # update the grid
+    for i in range(nb_rows):
+        grid.update_row(i, [Cell(v) for v in list(input())])
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
+    # if kirk is in the command room, update CR_reached
+    if grid.kirk_pos and grid.CR_pos and grid.kirk_pos.r == grid.CR_pos.r and grid.kirk_pos.c == grid.CR_pos.c:
+        grid.CR_reached = True
+
+    if grid.CR_reached:
+        # run out
+        next_cell = Point(r=kirk_row, c=kirk_col-1)
+    else:
+        if grid.CR_pos:
+            # go to the command room
+            path = a_star(grid.grid, grid.kirk_pos, grid.CR_pos)
+            next_cell = path[len(path) - 2]
+        else:
+            # keep exploring the grid to find the command room
+            next_cell = BFS(grid.grid, grid.kirk_pos, "?")[2]
 
     # Kirk's next move (UP DOWN LEFT or RIGHT).
-    print(find_direction((kirk_row, kirk_col), next_cell))
+    print(grid.find_direction(next_cell))
