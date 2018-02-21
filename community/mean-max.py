a4714bf5d839a485354abbfcd39b22610e479981
prudywsh <antoineprudhomme5@gmail.com>
2018-02-21 16:38:29 +0100

    mean max: code to detect collision moved into classes + Vector class created for spead + some trigo functions

--- community/mean-max.py
+++ community/mean-max.py
@@ -11,13 +11,100 @@ class Point(object):
         """
         return math.sqrt((self.x-p.x)**2 + (self.y-p.y)**2)
 
+    def on_segment(self, p, r):
+        """
+            Check if the Point is on segment 'pr'
+        """
+        return (self.x <= max(p.x, r.x) and self.x >= min(p.x, r.x) and
+            self.y <= max(p.y, r.y) and self.y >= min(p.y, r.y))
+
+    @staticmethod
+    def orientation(p, q, r):
+        """
+            0: colinear
+            1: clockwise
+            2: counterclockwise
+        """
+        val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+        if val == 0: return 0
+        return 1 if val > 0 else 2
+
+class Vector(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dot(self, v):
+        return (self.x*v.x)+(self.y*v.y)
+
+    def magnitude(self):
+        return math.sqrt((self.x)**2 + (self.y)**2)
+
+    def angle(self, v):
+        """ Return the angle in radian
+        """
+        return math.acos(self.dot(v) / (self.magnitude()*v.magnitude()))
+
 class Unit(Point):
     def __init__(self, unit_id, x, y, vx, vy, radius):
         Point.__init__(self, x, y)
         self.unit_id = unit_id
-        self.vx = vx
-        self.vy = vy
+        self.speed = Vector(vx, vy)
         self.radius = radius
+
+    def intersect(self, unit):
+        return self.distance(unit) < (self.radius + unit.radius)
+
+    def is_in(self, target):
+        """ Check if the center of this Unit is in the radius of the target
+        """
+        return self.distance(target) < target.radius
+
+    def will_collide_unit(self, unit, target):
+        unit_target = Point(0, 0)
+        if unit.speed.x > 0:
+            unit_target.x = math.floor((3000-unit.x)/unit.speed.x)
+        elif unit.speed.x < 0:
+            unit_target.x = math.floor((-3000-unit.x)/unit.speed.x)
+        if unit.speed.y > 0:
+            unit_target.y = math.floor((3000-unit.y)/unit.speed.y)
+        elif unit.speed.y < 0:
+            unit_target.y = math.floor((-3000-unit.y)/unit.speed.y)
+
+        if unit_target.x == 0 or unit_target.y == 0:
+            return False
+        return self.do_intersect(target, unit, unit_target)
+
+    def will_collide_units(self, units, target):
+        for unit in units:
+            if self.will_collide_unit(unit, target):
+                return True
+        return False
+
+    def do_intersect(self, q1, p2, q2):
+        # Find the four orientations needed for general and
+        # special cases
+        o1 = Point.orientation(self, q1, p2)
+        o2 = Point.orientation(self, q1, q2)
+        o3 = Point.orientation(p2, q2, self)
+        o4 = Point.orientation(p2, q2, q1)
+        # general case
+        if o1 != o2 and o3 != o4:
+            return True
+        # Special Cases
+        # self, q1 and p2 are colinear and p2 lies on segment p1q1
+        if o1 == 0 and p2.on_segment(self, q1): return True
+        # self, q1 and p2 are colinear and q2 lies on segment p1q1
+        if o2 == 0 and q2.on_segment(self, q1): return True
+        # p2, q2 and self are colinear and self lies on segment p2q2
+        if o3 == 0 and self.on_segment(p2, q2): return True
+        # p2, q2 and q1 are colinear and q1 lies on segment p2q2
+        if o4 == 0 and q1.on_segment(p2, q2): return True
+        # Doesn't fall in any of the above cases
+        return False
+
+    def calculate_next_point(self):
+        return Point(self.x + self.speed.x, self.y + self.speed.y)
 
 class WaterCarrier(object):
     def __init__(self, quantity):
@@ -32,6 +119,15 @@ class Vehicule(Unit):
     def __init__(self, unit_id, x, y, vx, vy, radius, mass):
         Unit.__init__(self, unit_id, x, y, vx, vy, radius)
         self.mass = mass
+        self.max_throttle = 300
+
+    def throttle(self, target, distance):
+        thrust = 300
+        if distance < 2000:
+            thrust = 150
+        elif distance < target.radius:
+            thrust = 50
+        return thrust
 
 class Looter(Vehicule):
     def __init__(self, unit_id, x, y, vx, vy, radius, mass):
@@ -65,64 +161,6 @@ def closer(origin, destinations):
             response_distance = dist
     return response, response_distance
 
-def circle_collision(circle1, circle2):
-    return circle2.distance(circle2) < (circle1.radius + circle2.radius)
-
-def orientation(p, q, r):
-    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
-    # colinear
-    if val == 0:
-        return 0
-    return 1 if val > 0 else 2
-
-def on_segment(p, q, r):
-    return (q.x <= max(p.x, r.x) and q.x >= min(p.x, r.x) and
-        q.y <= max(p.y, r.y) and q.y >= min(p.y, r.y))
-
-def do_intersect(p1, q1, p2, q2):
-    # Find the four orientations needed for general and
-    # special cases
-    o1 = orientation(p1, q1, p2)
-    o2 = orientation(p1, q1, q2)
-    o3 = orientation(p2, q2, p1)
-    o4 = orientation(p2, q2, q1)
-    # general case
-    if o1 != o2 and o3 != o4:
-        return True
-    # Special Cases
-    # p1, q1 and p2 are colinear and p2 lies on segment p1q1
-    if o1 == 0 and on_segment(p1, p2, q1): return True
-    # p1, q1 and p2 are colinear and q2 lies on segment p1q1
-    if o2 == 0 and on_segment(p1, q2, q1): return True
-    # p2, q2 and p1 are colinear and p1 lies on segment p2q2
-    if o3 == 0 and on_segment(p2, p1, q2): return True
-    # p2, q2 and q1 are colinear and q1 lies on segment p2q2
-    if o4 == 0 and on_segment(p2, q1, q2): return True
-    # Doesn't fall in any of the above cases
-    return False
-
-def collide(u1, u2, target):
-    # draw path of u2 given his current position and speed
-    end = Point(0, 0)
-    if u2.vx != 0:
-        if u2.vx > 0:
-            end.x = math.floor((3000-u2.x)/u2.vx)
-        else:
-            end.x = math.floor((-3000-u2.x)/u2.vx)
-    if u2.vy != 0:
-        if u2.vy > 0:
-            end.y = math.floor((3000-u2.y)/u2.vy)
-        else:
-            end.y = math.floor((-3000-u2.y)/u2.vy)
-
-    return do_intersect(u1, target, u2, end)
-
-def is_path_blocked(origin, units, target):
-    for unit in units:
-        if collide(origin, unit, target):
-            return True
-    return False
-
 def wreck_heuristic(reapers, wreck, tankers, destroyers, doofs):
     # water quantity
     value = wreck.quantity * -1000
@@ -132,7 +170,7 @@ def wreck_heuristic(reapers, wreck, tankers, destroyers, doofs):
     value += 5000 if enemy_dist < me_dist else -5000
     value += me_dist
     # check if the path to this wreck is blocked
-    if is_path_blocked(reapers[0], reapers[1:] + tankers + destroyers + doofs, wreck):
+    if reapers[0].will_collide_units(reapers[1:] + tankers + destroyers + doofs, wreck):
         value += 10000
     return value
 
@@ -185,31 +223,30 @@ while True:
             wrecks.append(Wreck(unit_id, x, y, vx, vy, radius, extra))
 
     # DESTROYER
-    # if close enought, throw grenade to tanker
-    # else, chase the closest tanker to my reaper ?
+    # find the closest tanker to
     DESTROYER = "WAIT"
-    closest_wreck, closest_wreck_dist = wrecks_heuristic(reapers, wrecks, tankers, destroyers, doofs)
-    closer_tanker, closer_tanker_dist = closer(destroyers[0], tankers)
-    if closer_tanker_dist < 2000 and rages[0] >= 60:
-        DESTROYER = "SKILL %d %d" % (closer_tanker.x+50, closer_tanker.y+50)
-    elif closest_wreck_dist < closer_tanker_dist:
-        DESTROYER = "%d %d %d" % (closest_wreck.x, closest_wreck.y, 300)
-    elif closer_tanker:
-        DESTROYER = "%d %d %d" % (closer_tanker.x, closer_tanker.y, 300)
+    closest_tanker, closest_tanker_dist = closer(reapers[0], tankers)
+    if closest_tanker_dist < 2000 and rages[0] >= 60:
+        DESTROYER = "SKILL %d %d" % (closest_tanker.x+50, closest_tanker.y+50)
+    elif closest_tanker:
+        DESTROYER = "%d %d %d" % (closest_tanker.x, closest_tanker.y, destroyers[0].throttle(closest_tanker, closest_tanker_dist))
 
     # REAPER
     # if there is a wreck, go
     # else, follow the destroyer
     REAPER = "WAIT"
-    closest_wreck, _ = wrecks_heuristic(reapers, wrecks, tankers, destroyers, doofs)
+    closest_wreck, closest_wreck_dist  = wrecks_heuristic(reapers, wrecks, tankers, destroyers, doofs)
     if closest_wreck:
-        REAPER = "%d %d %d" % (closest_wreck.x, closest_wreck.y, 300)
+        throttle = reapers[0].throttle(closest_wreck, closest_wreck_dist)
+        if throttle > 0:
+            REAPER = "%d %d %d" % (closest_wreck.x, closest_wreck.y, throttle)
     elif DESTROYER != "WAIT":
-        REAPER = "%d %d %d" % (closer_tanker.x, closer_tanker.y, 250)
+        throttle = reapers[0].throttle(destroyers[0], destroyers[0].distance(reapers[0]))
+        if throttle > 0:
+            REAPER = "%d %d %d" % (destroyers[0].x, destroyers[0].y, throttle)
 
     # DOOF
-    # block best ennemy reaper
-    DOOF = "%d %d %d" % (reapers[0].x, reapers[1 if scores[1]>scores[2] else 2].y, 300)
+    DOOF = "%d %d %d" % (reapers[1 if scores[1]>scores[2] else 2].x, reapers[1 if scores[1]>scores[2] else 2].y, 300)
 
     print(REAPER)
     print(DESTROYER)
